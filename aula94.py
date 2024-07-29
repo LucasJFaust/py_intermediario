6890bd148a49cf76364f0c7970faae6319996d98
LucasJFaust <lucasjfm.eng@gmail.com>
2024-07-29 18:48:14 -0300

    aulas 90 a 94

---
+++ aula94.py
@@ -0,0 +1,6 @@
+# Generator expression
+# Iterables: # Tem a responsabilidade de deter os valores de maneira sequencial 
+# Iterators: Tem a responsabilidade de te entregar um valor por vez
+
+iterable = ['Eu', 'Tenho', '__iter__'] 
+iterator = iterable.__iter__() 
